07b6c1b0f93b76bb7994586d385eb65fe4b5a200
con-f-use <con-f-use@users.noreply.github.com>
2021-04-12 13:41:43 +0200

    docs: get release from package

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import graphepp
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +22,7 @@ copyright = "2021, Julius Wallnöfer"
 author = "Julius Wallnöfer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3"
+release = graphepp.__version__
 
 
 # -- General configuration ---------------------------------------------------
